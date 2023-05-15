4cfdc635ba2f7917a0d93f2d89d4aca195e63abc
Max <bespalov.m.9@yandex.ru>
2023-05-15 19:19:01 +0300

    working version

--- app/src/main/java/bes/max/moviesearcher/MainActivity.kt
+++ app/src/main/java/bes/max/moviesearcher/MainActivity.kt
@@ -2,18 +2,27 @@ package bes.max.moviesearcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import bes.max.moviesearcher.model.Movie
+import bes.max.moviesearcher.model.MovieResponse
 import bes.max.moviesearcher.network.MovieApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var recyclerView: RecyclerView
-    lateinit var editText: EditText
-    lateinit var button: Button
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+    val listOfMovies = mutableListOf<Movie>()
+    private val adapter = MovieListAdapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,14 +34,48 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.button_main_activity)
 
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerView.adapter = MovieListAdapter()
+        recyclerView.adapter = adapter
 
         button.setOnClickListener {
             if (editText.text.isNotEmpty()) {
-
+                getMovies(editText.text.toString())
+                adapter.movies = listOfMovies
+                adapter.notifyDataSetChanged()
+            }
+            val inputMethodManager =
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            val view = this.currentFocus
+            if (view != null) {
+                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0)
             }
         }
+    }
 
-        MovieApi.movieRetrofitService
+    private fun getMovies(userInput: String) {
+        MovieApi.movieRetrofitService.getMovies(userInput).enqueue(object: Callback<MovieResponse> {
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                if (response.code() == 200) {
+                    listOfMovies.clear()
+                    adapter.notifyDataSetChanged()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        listOfMovies.addAll(response.body()!!.results)
+                        adapter.notifyDataSetChanged()
+                    }
+                    if (response.body()?.results?.isEmpty() == true) {
+                        Toast.makeText(applicationContext, "По запросу ничего не найдено", Toast.LENGTH_LONG)
+                    }
+                } else {
+                    listOfMovies.clear()
+                    adapter.notifyDataSetChanged()
+                    Toast.makeText(applicationContext, "Что-то пошло не так, попробуйте ещё раз", Toast.LENGTH_LONG)
+                }
+            }
+
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                listOfMovies.clear()
+                adapter.notifyDataSetChanged()
+                Toast.makeText(applicationContext, "Ошибка: ${t.toString()}", Toast.LENGTH_LONG)            }
+
+        })
     }
 }
