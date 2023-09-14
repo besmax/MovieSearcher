61e75aac9e920c5e57a5b527dd2e11d47c46d37f
Max <bespalov.m.9@ya.ru>
2023-09-14 20:56:14 +0300

    adapter rewriting for 2 types of viewholders (in progress)

--- app/src/main/java/bes/max/moviesearcher/ui/cast/CastDiffItemCallback.kt
+++ app/src/main/java/bes/max/moviesearcher/ui/cast/CastDiffItemCallback.kt
@@ -3,10 +3,28 @@ package bes.max.moviesearcher.ui.cast
 import androidx.recyclerview.widget.DiffUtil
 import bes.max.moviesearcher.domain.models.MovieCastPerson
 
-class CastDiffItemCallback : DiffUtil.ItemCallback<MovieCastPerson>() {
-    override fun areItemsTheSame(oldItem: MovieCastPerson, newItem: MovieCastPerson): Boolean =
-        (oldItem == newItem)
+//class CastDiffItemCallback : DiffUtil.ItemCallback<MovieCastPerson>() {
+//    override fun areItemsTheSame(oldItem: MovieCastPerson, newItem: MovieCastPerson): Boolean =
+//        (oldItem == newItem)
+//
+//    override fun areContentsTheSame(oldItem: MovieCastPerson, newItem: MovieCastPerson): Boolean =
+//        (oldItem.id == newItem.id)
+//}
 
-    override fun areContentsTheSame(oldItem: MovieCastPerson, newItem: MovieCastPerson): Boolean =
-        (oldItem.id == newItem.id)
+class CastDiffItemCallback : DiffUtil.ItemCallback<MovieCastRVItem>() {
+    override fun areItemsTheSame(oldItem: MovieCastRVItem, newItem: MovieCastRVItem): Boolean {
+        return if(oldItem is MovieCastRVItem.HeaderTitle && newItem is MovieCastRVItem.HeaderTitle) {
+            oldItem.headerText == newItem.headerText
+        } else if(oldItem is MovieCastRVItem.PersonItem && newItem is MovieCastRVItem.PersonItem) {
+            oldItem.data.id == newItem.data.id
+        } else false
+    }
+
+    override fun areContentsTheSame(oldItem: MovieCastRVItem, newItem: MovieCastRVItem): Boolean {
+        return if(oldItem is MovieCastRVItem.HeaderTitle && newItem is MovieCastRVItem.HeaderTitle) {
+            oldItem.headerText == newItem.headerText
+        } else if(oldItem is MovieCastRVItem.PersonItem && newItem is MovieCastRVItem.PersonItem) {
+            oldItem.data == newItem.data
+        } else false
+    }
 }
--- app/src/main/java/bes/max/moviesearcher/ui/cast/CastListAdapter.kt
+++ app/src/main/java/bes/max/moviesearcher/ui/cast/CastListAdapter.kt
@@ -2,9 +2,18 @@ package bes.max.moviesearcher.ui.cast
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import bes.max.moviesearcher.R
 import bes.max.moviesearcher.domain.models.MovieCastPerson
 
-class CastListAdapter : ListAdapter<MovieCastPerson, CastViewHolder>(CastDiffItemCallback()) {
+class CastListAdapter : ListAdapter<MovieCastRVItem, CastViewHolder>(CastDiffItemCallback()) {
+
+    override fun getItemViewType(position: Int): Int {
+        return when (currentList[position]) {
+            is MovieCastRVItem.HeaderTitle -> R.layout.cast_list_item_header
+            is MovieCastRVItem.PersonItem -> R.layout.cast_list_item
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CastViewHolder =
         CastViewHolder.inflateFrom(parent)
 
